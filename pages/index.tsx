299e396015c45f5aebf835872391897725214dd5
貴雪❆ <71214511+javascriptjp@users.noreply.github.com>
2023-12-05 05:34:14 +0900

    add some things

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,26 +8,31 @@ import { Icon } from '@iconify/react';
 import { basePath } from "@/next.config"
 const BASE_PATH = basePath ? basePath : ""
 const Okudakun: NextPage = () => {
-    return <>
-        <div>
-            <div className="video-responsive">
-                <iframe
-                    width="853"
-                    height="480"
-                    src={`https://www.youtube.com/embed/ttGR2B3cZaY?autoplay=1&mute=1&playsinline=1&loop=1&controls=0&disablekb=1&playlist=ttGR2B3cZaY`}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    title="Embedded youtube"
-                    style={{
-                        position: "fixed",
-                        zIndex: "-2",
-                        width: "100vw",
-                        height: "100vh",
-                        filter: "brightness(50%) blur(3px)"
-                    }}
-                />
-            </div>
-        </div>
+    return <Box>
+        <iframe
+            width="853"
+            height="480"
+            src={`https://www.youtube.com/embed/ttGR2B3cZaY?autoplay=1&mute=1&playsinline=1&loop=1&controls=0&disablekb=1&playlist=ttGR2B3cZaY`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+            style={{
+                position: "fixed",
+                zIndex: "-2",
+                width: "100vw",
+                height: "100vh",
+                filter: "brightness(50%) blur(3px)"
+            }}
+        />
+
+        <Typography sx={{
+            position: "fixed",
+            bottom: "0",
+            right: "10px"
+        }}>
+            This site is a fan-made work
+            <Link href="https://github.com/javascriptjp"><IconButton> <Icon icon="mdi:github" height="1.5em" /></IconButton></Link>
+        </Typography>
         <Box sx={{ height: "100vh", width: "100vw", display: "flex", justifyContent: "center" }} >
             <Box sx={{
                 margin: "auto",
@@ -43,15 +48,15 @@ const Okudakun: NextPage = () => {
                     Okudakun
                 </Typography>
                 <Box>
-                    <Link href="https://twitter.com/okudakun1"><IconButton><Icon icon="simple-icons:x" height="1.5em"/></IconButton></Link>
-                    <Link href="https://www.youtube.com/@okudakun"><IconButton><Icon icon="simple-icons:youtubemusic" height="1.5em"/></IconButton></Link>
-                    <Link href="https://music.apple.com/jp/artist/okudakun/1561899508"><IconButton><Icon icon="simple-icons:applemusic" height="1.5em"/></IconButton></Link>
-                    <Link href="https://soundcloud.com/okudakun"><IconButton><Icon icon="mdi:soundcloud" height="1.5em"/></IconButton></Link>
-                    <Link href="https://open.spotify.com/intl-ja/artist/68PyDKhJdbocAHfwTKb3t6"><IconButton><Icon icon="mdi:spotify" height="1.5em"/></IconButton></Link>
+                    <Link href="https://twitter.com/okudakun1"><IconButton><Icon icon="simple-icons:x" height="1.5em" /></IconButton></Link>
+                    <Link href="https://www.youtube.com/@okudakun"><IconButton><Icon icon="simple-icons:youtubemusic" height="1.5em" /></IconButton></Link>
+                    <Link href="https://music.apple.com/jp/artist/okudakun/1561899508"><IconButton><Icon icon="simple-icons:applemusic" height="1.5em" /></IconButton></Link>
+                    <Link href="https://soundcloud.com/okudakun"><IconButton><Icon icon="mdi:soundcloud" height="1.5em" /></IconButton></Link>
+                    <Link href="https://open.spotify.com/intl-ja/artist/68PyDKhJdbocAHfwTKb3t6"><IconButton><Icon icon="mdi:spotify" height="1.5em" /></IconButton></Link>
                 </Box>
             </Box>
         </Box>
-    </>
+    </Box>
 }
 
 export default Okudakun
