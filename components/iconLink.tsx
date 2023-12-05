b41d0fab490907df184d49e9260f08cab52a2ef4
貴雪❆ <71214511+javascriptjp@users.noreply.github.com>
2023-12-05 11:58:21 +0900

    Add IconLink Animation

--- components/iconLink.tsx
+++ components/iconLink.tsx
@@ -1,5 +1,17 @@
 import Link from 'next/link'
 import IconButton from '@mui/material/IconButton'
-const IconLink = (props: { url: string, icon: React.ReactNode }) => <Link href={props.url}><IconButton>{props.icon}</IconButton></Link>
+
+const buttonStyle = {
+    transition: "transform 0.2s ease-in-out",
+    "&:hover": {
+        transform: "scale3d(1.1, 1.1, 1)"
+    },
+}
+
+const IconLink = (props: { url: string, icon: React.ReactNode }) => {
+    return <Link href={props.url}>
+        <IconButton sx={buttonStyle}>{props.icon}</IconButton>
+    </Link>
+}
 
 export default IconLink
